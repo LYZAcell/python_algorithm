1940c417dbdbe7268ffa32f81434e339cd222040
Gayeong Lee_202002345 <61495016+LYZAcell@users.noreply.github.com>
2024-02-12 16:27:19 +0900

    4주차 실습
    문자열 포맷팅

--- Goorm_컴사/4_1_이스케이프시퀀스.py
+++ Goorm_컴사/4_1_이스케이프시퀀스.py
@@ -1,3 +1,14 @@
 #이스케이프 시퀀스 \',\",\t,\n
 str1 ="\'비구름\'이 말했습니다.\n\t\"비가 내리면 먼지가 사라질거야.\""
 print(str1)
+
+#문자열 포맷팅
+year=int(input())
+month=int(input())
+date=int(input())
+day=input()
+sentence1=("오늘은 {0}년 {1}월 {2}일 {3}입니다.".format(year, month, date, day))
+print(sentence1)
+#진짜 포멧팅은 아래의 방식이다.
+sentence1=(f"오늘은 {year}년 {month}월 {date}일 {day}입니다.")
+print(sentence1)
